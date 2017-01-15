fcd593c4e500c53205ff0cf63bdaf6b21005fafc
Taylor Monacelli <taylor.monacelli@streambox.com>
2017-01-15 14:12:37 -0800

    Boilerplate noise

---
+++ README.md
@@ -0,0 +1,4 @@
+# berks-test
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name 'berks-test'
+maintainer 'The Authors'
+maintainer_email 'taylor.monacelli@streambox.com'
+license 'all_rights'
+description 'Installs/Configures berks-test'
+long_description 'Installs/Configures berks-test'
+version '0.1.0'
+
+issues_url 'https://github.com/taylormonacelli/berks-test/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/taylormonacelli/berks-test' if respond_to?(:source_url)
