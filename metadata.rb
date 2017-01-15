0cfed68271ffd4b42a2d515319a4e97bb3d2ed3a
Taylor Monacelli <taylor.monacelli@streambox.com>
2017-01-15 14:39:46 -0800

    Add elasticsearch as dependency

--- metadata.rb
+++ metadata.rb
@@ -8,3 +8,5 @@ version '0.1.0'
 
 issues_url 'https://github.com/taylormonacelli/berks-test/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/taylormonacelli/berks-test' if respond_to?(:source_url)
+
+depends 'elasticsearch'
