52a5f9be0379f3e1f520242dc856ca3b9ae4c235
Taylor Monacelli <taylor.monacelli@streambox.com>
2017-01-15 20:03:21 -0800

    Still sources are same using explicit sources

--- Berksfile
+++ Berksfile
@@ -2,4 +2,4 @@ source 'https://supermarket.chef.io'
 
 metadata
 
-cookbook 'elasticsearch', '= 3.0.2'
+cookbook 'elasticsearch', '= 3.0.2', git: 'git://github.com/elastic/cookbook-elasticsearch'
--- Policyfile.rb
+++ Policyfile.rb
@@ -1,4 +1,4 @@
 name "example-application-service"
 default_source :supermarket
 run_list "elasticsearch::default"
-cookbook "elasticsearch", "= 3.0.2"
+cookbook "elasticsearch", "= 3.0.2", github: "elastic/cookbook-elasticsearch"
